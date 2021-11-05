f21bf939a663713b51b7c1a4d954ba6a6a54a52d
Chifane Laurian- Stefan <stefan@webproductionlabs.com>
2021-11-05 19:40:22 +0200

    mac - first push - started new structure

---
+++ src/app/sections/mobile/sesizari/sesizari.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sesizari',
+  templateUrl: './sesizari.component.html',
+  styleUrls: ['./sesizari.component.sass']
+})
+export class SesizariComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/sections/registratura/registratura.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegistraturaComponent } from './registratura.component';
+
+
+
+@NgModule({
+  declarations: [
+    RegistraturaComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RegistraturaModule { }
